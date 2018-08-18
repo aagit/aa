10b38728fa74209e57af27749c3b148567f2c248
Andrea Arcangeli <aarcange@redhat.com>
2018-08-17 22:01:48 -0400

    mm: thp: consolidate policy_nodemask call
    Just a minor cleanup.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -2183,6 +2183,8 @@ alloc_pages_vma(gfp_t gfp, int order, struct vm_area_struct *vma,
 		goto out;
 	}
 
+	nmask = policy_nodemask(gfp, pol);
+
 	if (unlikely(IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE) && hugepage)) {
 		int hpage_node = node;
 
@@ -2199,7 +2201,6 @@ alloc_pages_vma(gfp_t gfp, int order, struct vm_area_struct *vma,
 		if (pol->mode == MPOL_PREFERRED && !(pol->flags & MPOL_F_LOCAL))
 			hpage_node = pol->v.preferred_node;
 
-		nmask = policy_nodemask(gfp, pol);
 		if (!nmask || node_isset(hpage_node, *nmask)) {
 			mpol_cond_put(pol);
 			/*
@@ -2223,7 +2224,6 @@ alloc_pages_vma(gfp_t gfp, int order, struct vm_area_struct *vma,
 		}
 	}
 
-	nmask = policy_nodemask(gfp, pol);
 	preferred_nid = policy_node(gfp, pol, node);
 	page = __alloc_pages_nodemask(gfp, order, preferred_nid, nmask);
 	mpol_cond_put(pol);
