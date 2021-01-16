5cc57020ba97e2db13a1869faf220e4b28f06d54
Andrea Arcangeli <aarcange@redhat.com>
2021-01-15 21:18:22 -0500

    gup: FOLL_UNSHARE: copy-on-read fault
    There two cases where the Linux MM always handled the GUP pins wrong:
    
    a) GUP before fork()
    
    b) GUP after fork()
    
    The case (a) is well known as the "fork vs gup vs thread" race condition:
    
        https://lkml.kernel.org/r/20090311165833.GI27823@random.random
    
    Case (a) has more mild security implications because the parent
    process is in control, and it can prevent it from happening in many
    ways, for example by stopping all O_DIRECT until the child executed
    exec().
    
    The case (b) was first reported by Jann Horn on 27 May 2020 and it has
    greater security implications because it can happen out of control of
    the parent process.
    
    This secures case (b) by un-sharing shared anonymous pages from COW
    mappings, before taking any GUP pin.
    
    This leaves unchanged PageAnon for KSM and hugetlbfs.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -464,6 +464,7 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_REMOTE			0x80
 #define FAULT_FLAG_INSTRUCTION  		0x100
 #define FAULT_FLAG_INTERRUPTIBLE		0x200
+#define FAULT_FLAG_UNSHARE			0x400
 
 /*
  * The default fault flags that should be used by most of the
@@ -849,6 +850,15 @@ static inline int page_mapcount(struct page *page)
 	if (unlikely(PageCompound(page)))
 		return __page_mapcount(page);
 	return atomic_read(&page->_mapcount) + 1;
+}
+
+extern bool __page_anon_shared_irqsafe(struct page *);
+
+static bool page_anon_shared_irqsafe(struct page *page)
+{
+	if (unlikely(PageCompound(page)))
+		return __page_anon_shared_irqsafe(page);
+	return atomic_read(&page->_mapcount) > 0;
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
@@ -2802,6 +2812,7 @@ struct page *follow_page(struct vm_area_struct *vma, unsigned long address,
 #define FOLL_SPLIT_PMD	0x20000	/* split huge pmd before returning */
 #define FOLL_PIN	0x40000	/* pages must be released via unpin_user_page */
 #define FOLL_FAST_ONLY	0x80000	/* gup_fast: prevent fall-back to slow gup */
+#define FOLL_UNSHARE	0x100000/* gup: unshare anon page with mapcount > 1 */
 
 /*
  * FOLL_PIN and FOLL_LONGTERM may be used in various combinations with each
@@ -2868,6 +2879,48 @@ static inline int vm_fault_to_errno(vm_fault_t vm_fault, int foll_flags)
 	if (vm_fault & (VM_FAULT_SIGBUS | VM_FAULT_SIGSEGV))
 		return -EFAULT;
 	return 0;
+}
+
+/* false positives allowed, false negatives not allowed */
+static inline bool gup_page_unshare_irqsafe(unsigned int flags,
+					    struct page *page,
+					    bool is_head)
+{
+	if (flags & FOLL_WRITE)
+		return false;
+	if (!PageAnon(page))
+		return false;
+	if (PageKsm(page)) /* FIXME */
+		return false;
+	if (PageHuge(page)) /* FIXME */
+		return false;
+	if (is_head) {
+		if (PageTransHuge(page))
+			return page_trans_huge_anon_shared_irqsafe(page);
+		BUG();
+	} else
+		return page_anon_shared_irqsafe(page);
+}
+
+/* requires full accuracy */
+static inline bool gup_page_unshare(unsigned int flags,
+				    struct page *page,
+				    bool is_head)
+{
+	if (flags & FOLL_WRITE)
+		return false;
+	if (!PageAnon(page))
+		return false;
+	if (PageKsm(page)) /* FIXME */
+		return false;
+	if (PageHuge(page)) /* FIXME */
+		return false;
+	if (is_head) {
+		if (PageTransHuge(page))
+			return page_trans_huge_anon_shared(page);
+		BUG();
+	} else
+		return page_mapcount(page) > 1;
 }
 
 typedef int (*pte_fn_t)(pte_t *pte, unsigned long addr, void *data);
