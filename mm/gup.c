03126d48b56ecf995c03544a0c1bc0409b2fa126
Andrea Arcangeli <aarcange@redhat.com>
2021-01-15 21:18:22 -0500

    mm: gup: gup_must_unshare()
    Introduce gup_must_unshare(): the GUP logic that decides on which
    pages to activate FOLL_UNSHARE (and in turn the COR fault with
    FAULT_FLAG_UNSHARE) while taking readonly page pins on all
    write-protected pages.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/gup.c
+++ mm/gup.c
@@ -29,6 +29,67 @@ struct follow_page_context {
 	unsigned int page_mask;
 };
 
+static __always_inline bool is_fast_only_in_irq(bool irq_safe)
+{
+	/*
+	 * If irq_safe == true, we can still spin on the mapcount
+	 * seqlock as long as we're not in irq context. Only the
+	 * gup/pin_fast_only() can be invoked in irq context. This
+	 * means all gup/pin_fast() will always obtain an accurate
+	 * reading of the mapcount.
+	 */
+	return irq_safe && unlikely(!!irq_count());
+}
+
+/*
+ * For a page wrprotected in the pgtable, which pages do we need to
+ * unshare with copy-on-read (COR) for the GUP pin to remain coherent
+ * with the MM?
+ *
+ * This only provides full coherency to short term pins: FOLL_LONGTERM
+ * still needs to specify FOLL_WRITE|FOLL_FORCE in the caller and in
+ * turn it still risks inefficiency and to lose coherency with the MM
+ * in various cases.
+ */
+static __always_inline bool __gup_must_unshare(unsigned int flags,
+					       struct page *page,
+					       bool is_head, bool irq_safe)
+{
+	if (flags & FOLL_WRITE)
+		return false;
+	if (!PageAnon(page))
+		return false;
+	if (PageKsm(page))
+		return false;
+	if (PageHuge(page)) /* FIXME */
+		return false;
+	if (is_head) {
+		if (PageTransHuge(page)) {
+			if (!is_fast_only_in_irq(irq_safe))
+				return page_trans_huge_anon_shared(page);
+			return page_trans_huge_anon_shared_irqsafe(page);
+		}
+		BUG();
+	} else {
+		if (!is_fast_only_in_irq(irq_safe))
+			return page_mapcount(page) > 1;
+		return page_anon_shared_irqsafe(page);
+	}
+}
+
+/* requires full accuracy */
+bool gup_must_unshare(unsigned int flags, struct page *page, bool is_head)
+{
+	return __gup_must_unshare(flags, page, is_head, false);
+}
+
+/* false positives are allowed, false negatives not allowed */
+bool gup_must_unshare_irqsafe(unsigned int flags, struct page *page,
+			      bool is_head)
+{
+	return __gup_must_unshare(flags, page, is_head, true);
+}
+
 static void hpage_pincount_add(struct page *page, int refs)
 {
 	VM_BUG_ON_PAGE(!hpage_pincount_available(page), page);
