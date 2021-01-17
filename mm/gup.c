ed8f39eb50ce2499e17c345ca70a2adc80fe2a00
Andrea Arcangeli <aarcange@redhat.com>
2021-01-17 13:51:49 -0500

    mm: gup: FOLL_MM_SYNC: zeropage and MAP_PRIVATE pagecache
    This removes the need of FOLL_FORCE|FOLL_WRITE for MAP_PRIVATE
    pagecache and zeropages on anonymous mappings.
    
    If compared to FOLL_FORCE|FOLL_WRITE, FOLL_MM_SYNC will avoid
    unnecessary COWs and it'll retain MM coherency also for MAP_SHARED
    PROT_READ.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/gup.c
+++ mm/gup.c
@@ -101,7 +101,8 @@ static bool gup_must_unshare_hugetlbfs_slowpath(struct page *page)
  */
 static __always_inline bool __gup_must_unshare(unsigned int flags,
 					       struct page *page,
-					       bool is_head, bool irq_safe)
+					       bool is_head, bool irq_safe,
+					       struct vm_area_struct *vma)
 {
 	if (flags & (FOLL_WRITE|FOLL_NOUNSHARE))
 		return false;
@@ -109,7 +110,8 @@ static __always_inline bool __gup_must_unshare(unsigned int flags,
 	if (!(flags & (FOLL_GET|FOLL_PIN)))
 		return false;
 	if (!PageAnon(page))
-		return false;
+		return (flags & FOLL_MM_SYNC) &&
+			(irq_safe || !(vma->vm_flags & VM_SHARED));
 	if (PageKsm(page))
 		return gup_must_unshare_ksm(flags);
 	if (PageHuge(page)) {
@@ -138,16 +140,17 @@ static __always_inline bool __gup_must_unshare(unsigned int flags,
 }
 
 /* requires full accuracy */
-bool gup_must_unshare(unsigned int flags, struct page *page, bool is_head)
+bool gup_must_unshare(unsigned int flags, struct page *page, bool is_head,
+		      struct vm_area_struct *vma)
 {
-	return __gup_must_unshare(flags, page, is_head, false);
+	return __gup_must_unshare(flags, page, is_head, false, vma);
 }
 
 /* false positives are allowed, false negatives not allowed */
 bool gup_must_unshare_irqsafe(unsigned int flags, struct page *page,
 			      bool is_head)
 {
-	return __gup_must_unshare(flags, page, is_head, true);
+	return __gup_must_unshare(flags, page, is_head, true, NULL);
 }
 
 static void hpage_pincount_add(struct page *page, int refs)
@@ -712,7 +715,7 @@ retry:
 	 * exclusive.
 	 */
 	if (!pte_write(pte) &&
-	    gup_must_unshare(flags, page, false)) {
+	    gup_must_unshare(flags, page, false, vma)) {
 		page = ERR_PTR(-EMLINK);
 		goto out;
 	}
