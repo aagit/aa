825240c5a711e286ac769a03d1407bf6b6cd93a1
Andrea Arcangeli <aarcange@redhat.com>
2021-01-23 18:12:45 -0500

    mm: thp: page_trans_huge_mapcount_lock: optimize the lock_page_memcg
    Solve the lock inversion between lock_page_memcg and
    page_trans_huge_mapcount_unlock by moving the __dec_lruvec_page_state
    after the page_trans_huge_mapcount_unlock, so the lock_page_memcg
    remains optional.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2103,6 +2103,7 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 	bool young, write, soft_dirty, pmd_migration = false, uffd_wp = false;
 	unsigned long addr;
 	int i;
+	bool dec_page_state;
 
 	VM_BUG_ON(haddr & ~HPAGE_PMD_MASK);
 	VM_BUG_ON_VMA(vma->vm_start > haddr, vma);
@@ -2204,14 +2205,8 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 	pgtable = pgtable_trans_huge_withdraw(mm, pmd);
 	pmd_populate(mm, &_pmd, pgtable);
 
-	/*
-	 * lock_page_memcg() is taken before page_trans_huge_mapcount_lock() in
-	 * page_remove_anon_compound_rmap().
-	 */
-	if (!pmd_migration) {
-		lock_page_memcg(page);
+	if (!pmd_migration)
 		page_trans_huge_mapcount_lock(page);
-	}
 
 	for (i = 0, addr = haddr; i < HPAGE_PMD_NR; i++, addr += PAGE_SIZE) {
 		pte_t entry, *pte;
@@ -2248,6 +2243,7 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 		pte_unmap(pte);
 	}
 
+	dec_page_state = false;
 	if (!pmd_migration) {
 		/*
 		 * Set PG_double_map before dropping compound_mapcount to avoid
@@ -2261,7 +2257,7 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 
 		if (atomic_add_negative(-1, compound_mapcount_ptr(page))) {
 			/* Last compound_mapcount is gone. */
-			__dec_lruvec_page_state(page, NR_ANON_THPS);
+			dec_page_state = true;
 			if (TestClearPageDoubleMap(page)) {
 				/* No need in mapcount reference anymore */
 				for (i = 0; i < HPAGE_PMD_NR; i++)
@@ -2275,9 +2271,13 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 		 * page_trans_huge_mapcount_unlock().
 		 */
 		page_trans_huge_mapcount_unlock(page);
-		unlock_page_memcg(page);
 	} else
 		smp_wmb(); /* make pte visible before pmd */
+	if (dec_page_state) {
+		lock_page_memcg(page);
+		__dec_lruvec_page_state(page, NR_ANON_THPS);
+		unlock_page_memcg(page);
+	}
 	pmd_populate(mm, pmd, pgtable);
 
 	if (freeze) {
