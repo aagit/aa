4c0cb884f97044ded51db0e2e7481eb0a42c3eb8
Andrea Arcangeli <aarcange@redhat.com>
2021-01-27 02:54:40 -0500

    mm: thp: cleanup and optimize compound_nr
    Minor cleanup and minor optimizations to compound_nr and its callers.
    
    There's no reason to return "unsigned long" in compound_nr, when it's
    defined as "unsigned int" in the page structure and it's occasionally
    truncated back to a 32bit value in its callers.
    
    In addition its retval should be allowed to be cached in a caller
    saved register and not be forced to be re-read from memory at every
    iteration if any function called in the loop isn't in scope or inline.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -239,7 +239,7 @@ static inline void zero_user_segments(struct page *page,
 		unsigned start2, unsigned end2)
 {
 	void *kaddr = kmap_local_page(page);
-	unsigned int i;
+	unsigned int i, nr;
 
 	BUG_ON(end1 > page_size(page) || end2 > page_size(page));
 
@@ -250,7 +250,8 @@ static inline void zero_user_segments(struct page *page,
 		memset(kaddr + start2, 0, end2 - start2);
 
 	kunmap_local(kaddr);
-	for (i = 0; i < compound_nr(page); i++)
+	nr = compound_nr(page);
+	for (i = 0; i < nr; i++)
 		flush_dcache_page(page + i);
 }
 #endif
