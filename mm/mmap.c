71d97ba16dde3101cebbc3f11f226b777f88471d
Andrea Arcangeli <aarcange@redhat.com>
2021-05-19 18:55:21 -0400

    mm: mm_take_all_locks: add cond_resched()
    Add cond_resched() to potentially long tight loops.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3589,6 +3589,7 @@ int mm_take_all_locks(struct mm_struct *mm)
 	mutex_lock(&mm_all_locks_mutex);
 
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
+		cond_resched();
 		if (signal_pending(current))
 			goto out_unlock;
 		if (vma->vm_file && vma->vm_file->f_mapping &&
@@ -3597,6 +3598,7 @@ int mm_take_all_locks(struct mm_struct *mm)
 	}
 
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
+		cond_resched();
 		if (signal_pending(current))
 			goto out_unlock;
 		if (vma->vm_file && vma->vm_file->f_mapping &&
@@ -3605,11 +3607,14 @@ int mm_take_all_locks(struct mm_struct *mm)
 	}
 
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
+		cond_resched();
 		if (signal_pending(current))
 			goto out_unlock;
 		if (vma->anon_vma)
-			list_for_each_entry(avc, &vma->anon_vma_chain, same_vma)
+			list_for_each_entry(avc, &vma->anon_vma_chain, same_vma) {
+				cond_resched();
 				vm_lock_anon_vma(mm, avc->anon_vma);
+			}
 	}
 
 	return 0;
@@ -3668,9 +3673,12 @@ void mm_drop_all_locks(struct mm_struct *mm)
 	BUG_ON(!mutex_is_locked(&mm_all_locks_mutex));
 
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
+		cond_resched();
 		if (vma->anon_vma)
-			list_for_each_entry(avc, &vma->anon_vma_chain, same_vma)
+			list_for_each_entry(avc, &vma->anon_vma_chain, same_vma) {
+				cond_resched();
 				vm_unlock_anon_vma(avc->anon_vma);
+			}
 		if (vma->vm_file && vma->vm_file->f_mapping)
 			vm_unlock_mapping(vma->vm_file->f_mapping);
 	}
