ce4121031305fddbd7e2de6250cb2d8470b6162b
Andrea Arcangeli <aarcange@redhat.com>
2021-05-21 16:47:47 -0400

    mm: mprotect: avoid spurious COW faults for exclusive anon pages in cow mapping
    For cow mappings (anon vmas and MAP_PRIVATE of filebacked vmas), if
    the page is anon, the mapcount shows the anon page is exclusive, if
    the vmas has VM_WRITE set we can mark the pte writable.
    
    In the THP path the page_mapcount is too slow, a boolean retval is
    allowed to return "non exclusive" without checking every subpage so
    page_trans_huge_anon_shared() is used as an optimization instead of
    page_mapcount() != 1, but otherwise they remain identical.
    
    The THP mapcount is already atomic, it uses the readonly seqlock
    internally, so there's no need to take any page lock in the mprotect
    fast path for the THP path to skip the spurious COWs on THPs.
    
    Original idea and patch posted here:
    
    https://lkml.kernel.org/r/20210429214801.2583336-1-pcc@google.com
    
    Fix for uffd-wp from Peter Xu:
    
    https://lkml.kernel.org/r/YKgCsmMVp3wNLLBB@t490s
    
    v2: fix false positive BUG_ON reported by "kernel test robot".
    
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Suggested-by: Peter Collingbourne <pcc@google.com>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1856,6 +1856,21 @@ bool move_huge_pmd(struct vm_area_struct *vma, unsigned long old_addr,
 	return false;
 }
 
+/* this checks if we're allowed to set the pgtable of a COW mapping writable */
+static inline bool is_cow_writable(unsigned long vm_flags,
+				   pmd_t pmd, bool prot_numa)
+{
+	struct page *page;
+	if (prot_numa)
+		return false;
+	if (!is_cow_mapping(vm_flags) || !(vm_flags & VM_WRITE))
+		return false;
+	if (pmd_uffd_wp(pmd))
+		return false;
+	page = pmd_page(pmd);
+	return PageTransHugeAnon(page) && !page_trans_huge_anon_shared(page);
+}
+
 /*
  * Returns
  *  - 0 if PMD could not be locked
@@ -1873,6 +1888,7 @@ int change_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 	bool prot_numa = cp_flags & MM_CP_PROT_NUMA;
 	bool uffd_wp = cp_flags & MM_CP_UFFD_WP;
 	bool uffd_wp_resolve = cp_flags & MM_CP_UFFD_WP_RESOLVE;
+	bool dirty_accountable = cp_flags & MM_CP_DIRTY_ACCT;
 
 	ptl = __pmd_trans_huge_lock(pmd, vma);
 	if (!ptl)
@@ -1953,8 +1969,13 @@ int change_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 		entry = pmd_clear_uffd_wp(entry);
 	}
 	ret = HPAGE_PMD_NR;
+	if ((dirty_accountable ||
+	     is_cow_writable(vma->vm_flags, entry, prot_numa)) &&
+	    pmd_dirty(entry) &&
+	    (pmd_soft_dirty(entry) || !(vma->vm_flags & VM_SOFTDIRTY))) {
+		entry = pmd_mkwrite(entry);
+	}
 	set_pmd_at(mm, addr, pmd, entry);
-	BUG_ON(vma_is_anonymous(vma) && !preserve_write && pmd_write(entry));
 unlock:
 	spin_unlock(ptl);
 	return ret;
