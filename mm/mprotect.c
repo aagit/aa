47b4b61d9a157c6d73a9e521c530aa02a267b4f1
Andrea Arcangeli <aarcange@redhat.com>
2021-05-21 16:47:47 -0400

    mm: mprotect: avoid spurious COW faults for exclusive anon pages in cow mapping
    For cow mappings (anon vmas and MAP_PRIVATE of filebacked vmas), if
    the page is anon, the mapcount+swapcount shows the anon page is
    exclusive, if the vmas has VM_WRITE set we can mark the pte writable.
    
    In the THP path the page_mapcount is too slow, a boolean retval is
    allowed to return "non exclusive" without checking every subpage so
    page_trans_huge_anon_shared() is used as an optimization instead of
    page_mapcount() != 1, but otherwise they remain identical in design.
    
    The THP mapcount is already atomic, it uses the readonly seqlock
    internally, so there's no need to take any page lock in the mprotect
    fast path for the THP path to skip the spurious COWs on THPs.
    
    ====
     #define _GNU_SOURCE
     #include <sys/mman.h>
     #include <stdlib.h>
     #include <string.h>
     #include <stdio.h>
     #include <unistd.h>
    
     #define SIZE (1024*1024*1024)
    
    int main(int argc, char *argv[])
    {
    	char *p;
    	if (posix_memalign((void **)&p, sysconf(_SC_PAGESIZE)*512, SIZE))
    		perror("posix_memalign"), exit(1);
    	if (madvise(p, SIZE, argc > 1 ? MADV_HUGEPAGE : MADV_NOHUGEPAGE))
    		perror("madvise");
    	explicit_bzero(p, SIZE);
    	for (int loops = 0; loops < 40; loops++) {
    		if (mprotect(p, SIZE, PROT_READ))
    			perror("mprotect"), exit(1);
    		if (mprotect(p, SIZE, PROT_READ|PROT_WRITE))
    			perror("mprotect"), exit(1);
    		explicit_bzero(p, SIZE);
    	}
    }
    ====
    
    Stock 3 runs (lower is better):
    
    MADV_HUGEPAGE	1.7833 +- 0.0114 seconds time elapsed  ( +-  0.64% )
    MADV_NOHUGEPAGE	14.00 +- 1.06 seconds time elapsed  ( +-  7.60% )
    
    Patched 3 runs (lower is better):
    
    MADV_HUGEPAGE	1.7025 +- 0.0104 seconds time elapsed  ( +-  0.61% )
    MADV_NOHUGEPAGE	2.6165 +- 0.0182 seconds time elapsed  ( +-  0.69% )
    
    Original idea and patch posted here:
    
    https://lkml.kernel.org/r/20210429214801.2583336-1-pcc@google.com
    
    Fix for uffd-wp from Peter Xu:
    
    https://lkml.kernel.org/r/YKgCsmMVp3wNLLBB@t490s
    
    v2: fix false positive BUG_ON reported by "kernel test robot".
    
    v3: found and fixed memory corruption bug caused by the missing
        swapcount calculation in the original patch linked above that would
        trigger with swap enabled (that includes zram). This implementation
        inherited that same bug: page_mapcount == 1 check wasn't enough:
        reuse_swap_cache must be checked instead.
    
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Suggested-by: Peter Collingbourne <pcc@google.com>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -35,6 +35,30 @@
 
 #include "internal.h"
 
+/* this checks if we're allowed to set the pgtable of a COW mapping writable */
+static inline bool is_cow_writable(unsigned long vm_flags,
+				   pte_t pte, bool prot_numa)
+{
+	bool reuse;
+	struct page *page;
+	if (prot_numa)
+		return false;
+	if (!is_cow_mapping(vm_flags) || !(vm_flags & VM_WRITE))
+		return false;
+	if (pte_uffd_wp(pte))
+		return false;
+	page = pte_page(pte);
+	if (!PageAnonNoKsm(page))
+		return false;
+	if (page_mapcount(page) > 1)
+		return false;
+	if (!trylock_page(page))
+		return false;
+	reuse = reuse_swap_page(page, NULL);
+	unlock_page(page);
+	return reuse;
+}
+
 static unsigned long change_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 		unsigned long addr, unsigned long end, pgprot_t newprot,
 		unsigned long cp_flags)
@@ -132,7 +156,10 @@ static unsigned long change_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 			}
 
 			/* Avoid taking write faults for known dirty pages */
-			if (dirty_accountable && pte_dirty(ptent) &&
+			if ((dirty_accountable ||
+			     is_cow_writable(vma->vm_flags, ptent,
+					     prot_numa)) &&
+					pte_dirty(ptent) &&
 					(pte_soft_dirty(ptent) ||
 					 !(vma->vm_flags & VM_SOFTDIRTY))) {
 				ptent = pte_mkwrite(ptent);
