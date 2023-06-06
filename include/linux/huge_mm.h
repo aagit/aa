torvalds/linux

commit 6d0a07edd17cfc12fdc1f36de8072fa17cc3666f

mm: thp: calculate the mapcount correctly for THP pages during WP faults
This will provide fully accuracy to the mapcount calculation in the
write protect faults, so page pinning will not get broken by false
positive copy-on-writes.

total_mapcount() isn't the right calculation needed in
reuse_swap_page(), so this introduces a page_trans_huge_mapcount()
that is effectively the full accurate return value for page_mapcount()
if dealing with Transparent Hugepages, however we only use the
page_trans_huge_mapcount() during COW faults where it strictly needed,
due to its higher runtime cost.

This also provide at practical zero cost the total_mapcount
information which is needed to know if we can still relocate the page
anon_vma to the local vma. If page_trans_huge_mapcount() returns 1 we
can reuse the page no matter if it's a pte or a pmd_trans_huge
triggering the fault, but we can only relocate the page anon_vma to
the local vma->anon_vma if we're sure it's only this "vma" mapping the
whole THP physical range.

Kirill A. Shutemov discovered the problem with moving the page
anon_vma to the local vma->anon_vma in a previous version of this
patch and another problem in the way page_move_anon_rmap() was called.

Andrew Morton discovered that CONFIG_SWAP=n wouldn't build in a
previous version, because reuse_swap_page must be a macro to call
page_trans_huge_mapcount from swap.h, so this uses a macro again
instead of an inline function. With this change at least it's a less
dangerous usage than it was before, because "page" is used only once
now, while with the previous code reuse_swap_page(page++) would have
called page_mapcount on page+1 and it would have increased page twice
instead of just once.

Dean Luick noticed an uninitialized variable that could result in a
rmap inefficiency for the non-THP case in a previous version.

Mike Marciniszyn said:

: Our RDMA tests are seeing an issue with memory locking that bisects to
: commit 61f5d698cc97 ("mm: re-enable THP")
:

include/linux/mm.h
@@ -500,10 +500,19 @@ static inline int page_mapcount(struct page *page)
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 int total_mapcount(struct page *page);
+int page_trans_huge_mapcount(struct page *page, int *total_mapcount);
 #else
 static inline int total_mapcount(struct page *page)
 {
 	return page_mapcount(page);
+}
+static inline int page_trans_huge_mapcount(struct page *page,
+					   int *total_mapcount)
+{
+	int mapcount = page_mapcount(page);
+	if (total_mapcount)
+		*total_mapcount = mapcount;
+	return mapcount;
 }
 #endif
 

include/linux/swap.h
@@ -418,7 +418,7 @@ extern sector_t swapdev_block(int, pgoff_t);
 extern int page_swapcount(struct page *);
 extern int swp_swapcount(swp_entry_t entry);
 extern struct swap_info_struct *page_swap_info(struct page *);
-extern int reuse_swap_page(struct page *);
+extern bool reuse_swap_page(struct page *, int *);
 extern int try_to_free_swap(struct page *);
 struct backing_dev_info;
 
@@ -513,8 +513,8 @@ static inline int swp_swapcount(swp_entry_t entry)
 	return 0;
 }
 
-#define reuse_swap_page(page) \
-	(!PageTransCompound(page) && page_mapcount(page) == 1)
+#define reuse_swap_page(page, total_mapcount) \
+	(page_trans_huge_mapcount(page, total_mapcount) == 1)
 
 static inline int try_to_free_swap(struct page *page)
 {

mm/huge_memory.c
@@ -1298,15 +1298,9 @@ int do_huge_pmd_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	VM_BUG_ON_PAGE(!PageCompound(page) || !PageHead(page), page);
 	/*
 	 * We can only reuse the page if nobody else maps the huge page or it's
-	 * part. We can do it by checking page_mapcount() on each sub-page, but
-	 * it's expensive.
-	 * The cheaper way is to check page_count() to be equal 1: every
-	 * mapcount takes page reference reference, so this way we can
-	 * guarantee, that the PMD is the only mapping.
-	 * This can give false negative if somebody pinned the page, but that's
-	 * fine.
+	 * part.
 	 */
-	if (page_mapcount(page) == 1 && page_count(page) == 1) {
+	if (page_trans_huge_mapcount(page, NULL) == 1) {
 		pmd_t entry;
 		entry = pmd_mkyoung(orig_pmd);
 		entry = maybe_pmd_mkwrite(pmd_mkdirty(entry), vma);
@@ -2079,7 +2073,8 @@ static int __collapse_huge_page_isolate(struct vm_area_struct *vma,
 		if (pte_write(pteval)) {
 			writable = true;
 		} else {
-			if (PageSwapCache(page) && !reuse_swap_page(page)) {
+			if (PageSwapCache(page) &&
+			    !reuse_swap_page(page, NULL)) {
 				unlock_page(page);
 				result = SCAN_SWAP_CACHE_PAGE;
 				goto out;
@@ -3219,6 +3214,64 @@ int total_mapcount(struct page *page)
 		ret += atomic_read(&page[i]._mapcount) + 1;
 	if (PageDoubleMap(page))
 		ret -= HPAGE_PMD_NR;
+	return ret;
+}
+
+/*
+ * This calculates accurately how many mappings a transparent hugepage
+ * has (unlike page_mapcount() which isn't fully accurate). This full
+ * accuracy is primarily needed to know if copy-on-write faults can
+ * reuse the page and change the mapping to read-write instead of
+ * copying them. At the same time this returns the total_mapcount too.
+ *
+ * The function returns the highest mapcount any one of the subpages
+ * has. If the return value is one, even if different processes are
+ * mapping different subpages of the transparent hugepage, they can
+ * all reuse it, because each process is reusing a different subpage.
+ *
+ * The total_mapcount is instead counting all virtual mappings of the
+ * subpages. If the total_mapcount is equal to "one", it tells the
+ * caller all mappings belong to the same "mm" and in turn the
+ * anon_vma of the transparent hugepage can become the vma->anon_vma
+ * local one as no other process may be mapping any of the subpages.
+ *
+ * It would be more accurate to replace page_mapcount() with
+ * page_trans_huge_mapcount(), however we only use
+ * page_trans_huge_mapcount() in the copy-on-write faults where we
+ * need full accuracy to avoid breaking page pinning, because
+ * page_trans_huge_mapcount() is slower than page_mapcount().
+ */
+int page_trans_huge_mapcount(struct page *page, int *total_mapcount)
+{
+	int i, ret, _total_mapcount, mapcount;
+
+	/* hugetlbfs shouldn't call it */
+	VM_BUG_ON_PAGE(PageHuge(page), page);
+
+	if (likely(!PageTransCompound(page))) {
+		mapcount = atomic_read(&page->_mapcount) + 1;
+		if (total_mapcount)
+			*total_mapcount = mapcount;
+		return mapcount;
+	}
+
+	page = compound_head(page);
+
+	_total_mapcount = ret = 0;
+	for (i = 0; i < HPAGE_PMD_NR; i++) {
+		mapcount = atomic_read(&page[i]._mapcount) + 1;
+		ret = max(ret, mapcount);
+		_total_mapcount += mapcount;
+	}
+	if (PageDoubleMap(page)) {
+		ret -= 1;
+		_total_mapcount -= HPAGE_PMD_NR;
+	}
+	mapcount = compound_mapcount(page);
+	ret += mapcount;
+	_total_mapcount += mapcount;
+	if (total_mapcount)
+		*total_mapcount = _total_mapcount;
 	return ret;
 }
 

mm/memory.c
@@ -2373,6 +2373,7 @@ static int do_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	 * not dirty accountable.
 	 */
 	if (PageAnon(old_page) && !PageKsm(old_page)) {
+		int total_mapcount;
 		if (!trylock_page(old_page)) {
 			get_page(old_page);
 			pte_unmap_unlock(page_table, ptl);
@@ -2387,13 +2388,18 @@ static int do_wp_page(struct mm_struct *mm, struct vm_area_struct *vma,
 			}
 			put_page(old_page);
 		}
-		if (reuse_swap_page(old_page)) {
-			/*
-			 * The page is all ours.  Move it to our anon_vma so
-			 * the rmap code will not search our parent or siblings.
-			 * Protected against the rmap code by the page lock.
-			 */
-			page_move_anon_rmap(old_page, vma, address);
+		if (reuse_swap_page(old_page, &total_mapcount)) {
+			if (total_mapcount == 1) {
+				/*
+				 * The page is all ours. Move it to
+				 * our anon_vma so the rmap code will
+				 * not search our parent or siblings.
+				 * Protected against the rmap code by
+				 * the page lock.
+				 */
+				page_move_anon_rmap(compound_head(old_page),
+						    vma, address);
+			}
 			unlock_page(old_page);
 			return wp_page_reuse(mm, vma, address, page_table, ptl,
 					     orig_pte, old_page, 0, 0);
@@ -2617,7 +2623,7 @@ static int do_swap_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	inc_mm_counter_fast(mm, MM_ANONPAGES);
 	dec_mm_counter_fast(mm, MM_SWAPENTS);
 	pte = mk_pte(page, vma->vm_page_prot);
-	if ((flags & FAULT_FLAG_WRITE) && reuse_swap_page(page)) {
+	if ((flags & FAULT_FLAG_WRITE) && reuse_swap_page(page, NULL)) {
 		pte = maybe_mkwrite(pte_mkdirty(pte), vma);
 		flags &= ~FAULT_FLAG_WRITE;
 		ret |= VM_FAULT_WRITE;

mm/swapfile.c
@@ -922,18 +922,19 @@ out:
  * to it.  And as a side-effect, free up its swap: because the old content
  * on disk will never be read, and seeking back there to write new content
  * later would only waste time away from clustering.
+ *
+ * NOTE: total_mapcount should not be relied upon by the caller if
+ * reuse_swap_page() returns false, but it may be always overwritten
+ * (see the other implementation for CONFIG_SWAP=n).
  */
-int reuse_swap_page(struct page *page)
+bool reuse_swap_page(struct page *page, int *total_mapcount)
 {
 	int count;
 
 	VM_BUG_ON_PAGE(!PageLocked(page), page);
 	if (unlikely(PageKsm(page)))
-		return 0;
-	/* The page is part of THP and cannot be reused */
-	if (PageTransCompound(page))
-		return 0;
-	count = page_mapcount(page);
+		return false;
+	count = page_trans_huge_mapcount(page, total_mapcount);
 	if (count <= 1 && PageSwapCache(page)) {
 		count += page_swapcount(page);
 		if (count == 1 && !PageWriteback(page)) {

aagit/aa

commit e58adc528f692a3d14ca999885bcdf89373c5bed

mm: thp: fix COW accuracy on double map THP
David reported the do_wp_page() fault would erroenously copy a subpage
mapped on a double map THP, that was actually exclusive, because some
other subpage wasn't exclusive.

The page_trans_huge_map_swapcount() was doing the right math for the
whole THP that is used to decide if the THP COW can reuse the page
without splitting the pmd and falling back to non-THP COW, but it was
inaccurate and causing too much COWing if used to take the non-THP COW
decision.

Since the day write and longterm GUP pins existed, COWing too much is
always a bug that causes loss of synchronicity, so this longstanding
issue also causd a loss of synchronicity of the GUP pin.

The fix is simple and it just consists of doing the same math but
subpage granular if the caller is only interested to reuse the
subpage, not the whole THP.

Fixes: 6d0a07edd17c ("mm: thp: calculate the mapcount correctly for THP pages during WP faults")
Reported-by: David Hildenbrand <david@redhat.com>
Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>

include/linux/huge_mm.h
@@ -392,6 +392,11 @@ static inline void page_trans_huge_mapcount_unlock(struct page *page)
 	bit_spin_unlock(PG_locked, &page[1].flags);
 }
 
+static inline int page_trans_huge_subpage_idx(unsigned long address)
+{
+	return (address & ~HPAGE_PMD_MASK) >> PAGE_SHIFT;
+}
+
 #else /* CONFIG_TRANSPARENT_HUGEPAGE */
 #define HPAGE_PMD_SHIFT ({ BUILD_BUG(); 0; })
 #define HPAGE_PMD_MASK ({ BUILD_BUG(); 0; })
@@ -565,6 +570,11 @@ static inline void page_trans_huge_mapcount_lock(struct page *page)
 
 static inline void page_trans_huge_mapcount_unlock(struct page *page)
 {
+}
+
+static inline int page_trans_huge_subpage_idx(unsigned long address)
+{
+	return 0;
 }
 
 #endif /* CONFIG_TRANSPARENT_HUGEPAGE */

include/linux/mm.h
@@ -883,13 +883,14 @@ static inline int page_mapcount(struct page *page)
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 int total_mapcount(struct page *page);
-int page_trans_huge_mapcount(struct page *page, int *total_mapcount);
+int page_trans_huge_mapcount(struct page *page, int thp_idx,
+			     int *total_mapcount);
 #else
 static inline int total_mapcount(struct page *page)
 {
 	return page_mapcount(page);
 }
-static inline int page_trans_huge_mapcount(struct page *page,
+static inline int page_trans_huge_mapcount(struct page *page, int thp_idx,
 					   int *total_mapcount)
 {
 	int mapcount = page_mapcount(page);
@@ -2956,9 +2957,10 @@ static inline int vm_fault_to_errno(vm_fault_t vm_fault, int foll_flags)
 }
 
 extern bool gup_must_unshare(unsigned int flags, struct page *page,
-			     bool is_head, struct vm_area_struct *vma);
+			     unsigned long address, bool is_head,
+			     struct vm_area_struct *vma);
 extern bool gup_must_unshare_irqsafe(unsigned int flags, struct page *page,
-				     bool is_head);
+				     unsigned long address, bool is_head);
 
 typedef int (*pte_fn_t)(pte_t *pte, unsigned long addr, void *data);
 extern int apply_to_page_range(struct mm_struct *mm, unsigned long address,

include/linux/swap.h
@@ -506,8 +506,8 @@ extern int __swp_swapcount(swp_entry_t entry);
 extern int swp_swapcount(swp_entry_t entry);
 extern struct swap_info_struct *page_swap_info(struct page *);
 extern struct swap_info_struct *swp_swap_info(swp_entry_t entry);
-extern bool can_read_pin_swap_page(struct page *);
-extern bool reuse_swap_page(struct page *, int *);
+extern bool can_read_pin_swap_page(struct page *, int);
+extern bool reuse_swap_page(struct page *, int, int *);
 extern int try_to_free_swap(struct page *);
 struct backing_dev_info;
 extern int init_swap_address_space(unsigned int type, unsigned long nr_pages);
@@ -669,16 +669,19 @@ static inline int swp_swapcount(swp_entry_t entry)
 	return 0;
 }
 
-static inline bool can_read_pin_swap_page(struct page *page)
+static inline bool can_read_pin_swap_page(struct page *page,
+					  unsigned long thp_idx)
 {
-	return page_trans_huge_mapcount(page, NULL) <= 1;
+	return page_trans_huge_mapcount(page, thp_idx, NULL) <= 1;
 }
 
-static inline bool reuse_swap_page(struct page *page, int *total_map_swapcount)
+static inline bool reuse_swap_page(struct page *page, int thp_idx,
+				   int *total_map_swapcount)
 {
 	if (unlikely(PageKsm(page)))
 		return false;
-	return page_trans_huge_mapcount(page, total_map_swapcount) <= 1;
+	return page_trans_huge_mapcount(page, thp_idx,
+					total_map_swapcount) <= 1;
 }
 
 static inline int try_to_free_swap(struct page *page)
@@ -694,6 +697,28 @@ static inline swp_entry_t get_swap_page(struct page *page)
 }
 
 #endif /* CONFIG_SWAP */
+
+#define REUSE_SWAP_PAGE_WHOLE_THP -1
+#define REUSE_SWAP_PAGE_NO_THP -2
+
+static inline bool can_read_pin_swap_page_no_thp(struct page * page) {
+	return can_read_pin_swap_page(page, REUSE_SWAP_PAGE_NO_THP);
+}
+static inline bool can_read_pin_swap_page_addr(struct page * page,
+					       unsigned long addr) {
+	return can_read_pin_swap_page(page, page_trans_huge_subpage_idx(addr));
+}
+static inline bool reuse_swap_page_whole_thp(struct page * page,
+					     int *total_map_swapcount) {
+	return reuse_swap_page(page, REUSE_SWAP_PAGE_WHOLE_THP,
+			       total_map_swapcount);
+}
+static inline bool reuse_swap_page_addr(struct page * page,
+					unsigned long addr,
+					int *total_map_swapcount) {
+	return reuse_swap_page(page, page_trans_huge_subpage_idx(addr),
+			       total_map_swapcount);
+}
 
 #ifdef CONFIG_THP_SWAP
 extern int split_swap_cluster(swp_entry_t entry);

mm/gup.c
@@ -46,7 +46,8 @@ static __always_inline bool gup_must_unshare_ksm(unsigned int flags)
 	return !!(flags & FOLL_MM_SYNC);
 }
 
-static bool gup_must_unshare_slowpath(struct page *page, unsigned int flags)
+static bool __gup_must_unshare_slowpath(struct page *page, unsigned int flags,
+					unsigned long thp_idx)
 {
 	bool must_unshare;
 	/*
@@ -68,9 +69,17 @@ static bool gup_must_unshare_slowpath(struct page *page, unsigned int flags)
 		unlock_page(page);
 		return gup_must_unshare_ksm(flags);
 	}
-	must_unshare = !can_read_pin_swap_page(page);
+	must_unshare = !can_read_pin_swap_page(page, thp_idx);
 	unlock_page(page);
 	return must_unshare;
+}
+
+static __always_inline bool gup_must_unshare_slowpath(struct page *page,
+						      unsigned int flags,
+						      unsigned long address)
+{
+	int thp_idx = page_trans_huge_subpage_idx(address);
+	return __gup_must_unshare_slowpath(page, flags, thp_idx);
 }
 
 static bool gup_must_unshare_hugetlbfs_slowpath(struct page *page)
@@ -133,6 +142,7 @@ static bool gup_must_unshare_hugetlbfs_slowpath(struct page *page)
  */
 static __always_inline bool __gup_must_unshare(unsigned int flags,
 					       struct page *page,
+					       unsigned long address,
 					       bool is_head, bool irq_safe,
 					       struct vm_area_struct *vma)
 {
@@ -159,7 +169,8 @@ static __always_inline bool __gup_must_unshare(unsigned int flags,
 			if (!is_fast_only_in_irq(irq_safe)) {
 				if (page_trans_huge_anon_shared(page))
 					return true;
-				return gup_must_unshare_slowpath(page, flags);
+				return gup_must_unshare_slowpath(page, flags,
+								 address);
 			}
 			return true;
 		}
@@ -168,24 +179,26 @@ static __always_inline bool __gup_must_unshare(unsigned int flags,
 		if (!is_fast_only_in_irq(irq_safe)) {
 			if (page_mapcount(page) > 1)
 				return true;
-			return gup_must_unshare_slowpath(page, flags);
+			return gup_must_unshare_slowpath(page, flags,
+							 address);
 		}
 		return true;
 	}
 }
 
 /* requires full accuracy */
-bool gup_must_unshare(unsigned int flags, struct page *page, bool is_head,
+bool gup_must_unshare(unsigned int flags, struct page *page,
+		      unsigned long address, bool is_head,
 		      struct vm_area_struct *vma)
 {
-	return __gup_must_unshare(flags, page, is_head, false, vma);
+	return __gup_must_unshare(flags, page, address, is_head, false, vma);
 }
 
 /* false positives are allowed, false negatives not allowed */
 bool gup_must_unshare_irqsafe(unsigned int flags, struct page *page,
-			      bool is_head)
+			      unsigned long address, bool is_head)
 {
-	return __gup_must_unshare(flags, page, is_head, true, NULL);
+	return __gup_must_unshare(flags, page, address, is_head, true, NULL);
 }
 
 static void hpage_pincount_add(struct page *page, int refs)
@@ -750,7 +763,7 @@ retry:
 	 * exclusive.
 	 */
 	if (!pte_write(pte) &&
-	    gup_must_unshare(flags, page, false, vma)) {
+	    gup_must_unshare(flags, page, address, false, vma)) {
 		page = ERR_PTR(-EMLINK);
 		goto out;
 	}
@@ -2543,7 +2556,7 @@ static int gup_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 		}
 
 		if (!pte_write(pte) &&
-		    gup_must_unshare_irqsafe(flags, page, false)) {
+		    gup_must_unshare_irqsafe(flags, page, addr, false)) {
 			put_compound_head(head, 1, flags);
 			goto pte_unmap;
 		}
@@ -2831,7 +2844,7 @@ static int gup_huge_pmd(pmd_t orig, pmd_t *pmdp, unsigned long addr,
 		return 0;
 
 	if (!pmd_write(orig) &&
-	    gup_must_unshare_irqsafe(flags, head, true)) {
+	    gup_must_unshare_irqsafe(flags, head, addr, true)) {
 		put_compound_head(head, refs, flags);
 		return 0;
 	}

mm/huge_memory.c
@@ -1363,7 +1363,7 @@ vm_fault_t do_huge_pmd_wp_page(struct vm_fault *vmf)
 	 * We can only reuse the page if nobody else maps the huge page or it's
 	 * part.
 	 */
-	if (reuse_swap_page(page, NULL)) {
+	if (reuse_swap_page_whole_thp(page, NULL)) {
 		pmd_t entry;
 		entry = pmd_mkyoung(orig_pmd);
 		entry = maybe_pmd_mkwrite(pmd_mkdirty(entry), vma);
@@ -1418,7 +1418,7 @@ struct page *follow_trans_huge_pmd(struct vm_area_struct *vma,
 
 	/* see comments of the gup_must_unshare() callers in mm/gup.c */
 	if (!pmd_write(*pmd) &&
-	    gup_must_unshare(flags, page, true, vma))
+	    gup_must_unshare(flags, page, addr, true, vma))
 		return ERR_PTR(-EMLINK);
 
 	if (!try_grab_page(page, flags))
@@ -2583,7 +2583,8 @@ again:
  * need full accuracy to avoid breaking page pinning, because
  * page_trans_huge_mapcount() is slower than page_mapcount().
  */
-int page_trans_huge_mapcount(struct page *page, int *total_mapcount)
+int page_trans_huge_mapcount(struct page *page, int thp_idx,
+			     int *total_mapcount)
 {
 	int i, ret, _total_mapcount, mapcount;
 	unsigned int seqcount;
@@ -2592,11 +2593,14 @@ int page_trans_huge_mapcount(struct page *page, int *total_mapcount)
 	VM_BUG_ON_PAGE(PageHuge(page), page);
 
 	if (likely(!PageTransCompound(page))) {
+		WARN_ON_ONCE(thp_idx == REUSE_SWAP_PAGE_WHOLE_THP);
 		mapcount = atomic_read(&page->_mapcount) + 1;
 		if (total_mapcount)
 			*total_mapcount = mapcount;
 		return mapcount;
 	}
+
+	WARN_ON_ONCE(thp_idx == REUSE_SWAP_PAGE_NO_THP);
 
 	page = compound_head(page);
 
@@ -2605,7 +2609,8 @@ again:
 	_total_mapcount = ret = 0;
 	for (i = 0; i < thp_nr_pages(page); i++) {
 		mapcount = atomic_read(&page[i]._mapcount) + 1;
-		ret = max(ret, mapcount);
+		if (thp_idx < 0 || i == thp_idx)
+			ret = max(ret, mapcount);
 		_total_mapcount += mapcount;
 	}
 	if (PageDoubleMap(page)) {

mm/hugetlb.c
@@ -5553,7 +5553,7 @@ long follow_hugetlb_page(struct mm_struct *mm, struct vm_area_struct *vma,
 		    (!huge_pte_write(pteval) &&
 		     ((flags & FOLL_WRITE) ||
 		      (unshare = gup_must_unshare(flags, pte_page(pteval),
-						  true, vma))))) {
+						  vaddr, true, vma))))) {
 			vm_fault_t ret;
 			unsigned int fault_flags = 0;
 
@@ -6276,7 +6276,7 @@ retry:
 		 * check here is just in case.
 		 */
 		if (!huge_pte_write(pte) &&
-		    gup_must_unshare(flags, head_page, true, vma)) {
+		    gup_must_unshare(flags, head_page, address, true, vma)) {
 			page = NULL;
 			goto out;
 		}

mm/khugepaged.c
@@ -685,7 +685,7 @@ static int __collapse_huge_page_isolate(struct vm_area_struct *vma,
 			goto out;
 		}
 		if (!pte_write(pteval) && PageSwapCache(page) &&
-				!reuse_swap_page(page, NULL)) {
+		    !reuse_swap_page_whole_thp(page, NULL)) {
 			/*
 			 * Page is in the swap cache and cannot be re-used.
 			 * It cannot be collapsed into a THP.

mm/ksm.c
@@ -1092,7 +1092,7 @@ static int write_protect_page(struct vm_area_struct *vma, struct page *page,
 		 *
 		 * PageSwapCache() is stable too under page lock.
 		 */
-		if (can_read_pin_swap_page(page) &&
+		if (can_read_pin_swap_page_no_thp(page) &&
 		    2 + PageSwapCache(page) != page_count(page)) {
 			set_pte_at(mm, pvmw.address, pvmw.pte, entry);
 			goto out_unlock;
@@ -1199,7 +1199,7 @@ static int replace_page(struct vm_area_struct *vma, struct page *page,
 	 *
 	 * PageSwapCache() is stable too under page lock.
 	 */
-	if (can_read_pin_swap_page(page) &&
+	if (can_read_pin_swap_page_no_thp(page) &&
 	    2 + PageSwapCache(page) != page_count(page)) {
 		set_pte_at(mm, addr, ptep, oldpte);
 		pte_unmap_unlock(ptep, ptl);

mm/memory.c
@@ -3061,7 +3061,8 @@ static __always_inline vm_fault_t __wp_page_copy(struct vm_fault *vmf,
 			 * GUP R/O pin may already have been taken so
 			 * go back to GUP and try again.
 			 */
-			if (can_read_pin_swap_page(vmf->page)) {
+			if (can_read_pin_swap_page_addr(vmf->page,
+							vmf->address)) {
 				set_pte_at_notify(mm, vmf->address, vmf->pte,
 						  vmf->orig_pte);
 				goto unlock_old_page;
@@ -3341,7 +3342,8 @@ static vm_fault_t wp_page_unshare(struct vm_fault *vmf)
 			unlock_page(vmf->page);
 			goto out_unlock;
 		}
-		must_unshare = !can_read_pin_swap_page(vmf->page);
+		must_unshare = !can_read_pin_swap_page_addr(vmf->page,
+							    vmf->address);
 		unlock_page(vmf->page);
 		if (must_unshare)
 			return __wp_page_unshare(vmf);
@@ -3545,7 +3547,8 @@ static vm_fault_t do_wp_page(struct vm_fault *vmf)
 			wp_page_reuse(vmf);
 			return VM_FAULT_WRITE;
 		}
-		if (reuse_swap_page(vmf->page, &total_map_swapcount)) {
+		if (reuse_swap_page_addr(vmf->page, vmf->address,
+					 &total_map_swapcount)) {
 			if (total_map_swapcount == 1) {
 				/*
 				 * The page is all ours. Move it to
@@ -3910,7 +3913,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 	inc_mm_counter_fast(vma->vm_mm, MM_ANONPAGES);
 	dec_mm_counter_fast(vma->vm_mm, MM_SWAPENTS);
 	pte = mk_pte(page, vma->vm_page_prot);
-	if ((vmf->flags & FAULT_FLAG_WRITE) && reuse_swap_page(page, NULL)) {
+	if ((vmf->flags & FAULT_FLAG_WRITE) &&
+	    reuse_swap_page_addr(page, vmf->address, NULL)) {
 		pte = maybe_mkwrite(pte_mkdirty(pte), vma);
 		vmf->flags &= ~FAULT_FLAG_WRITE;
 		ret |= VM_FAULT_WRITE;

mm/swapfile.c
@@ -1603,7 +1603,8 @@ static bool page_swapped(struct page *page)
 	return false;
 }
 
-static int page_trans_huge_map_swapcount(struct page *page, int *total_mapcount,
+static int page_trans_huge_map_swapcount(struct page *page, int thp_idx,
+					 int *total_mapcount,
 					 int *total_swapcount)
 {
 	int i, map_swapcount, _total_mapcount, _total_swapcount;
@@ -1618,13 +1619,16 @@ static int page_trans_huge_map_swapcount(struct page *page, int *total_mapcount,
 	VM_BUG_ON_PAGE(PageHuge(page), page);
 
 	if (!IS_ENABLED(CONFIG_THP_SWAP) || likely(!PageTransCompound(page))) {
-		mapcount = page_trans_huge_mapcount(page, total_mapcount);
+		mapcount = page_trans_huge_mapcount(page, thp_idx,
+						    total_mapcount);
 		if (PageSwapCache(page))
 			swapcount = page_swapcount(page);
 		if (total_swapcount)
 			*total_swapcount = swapcount;
 		return mapcount + swapcount;
 	}
+
+	WARN_ON_ONCE(thp_idx == REUSE_SWAP_PAGE_NO_THP);
 
 	page = compound_head(page);
 
@@ -1652,7 +1656,9 @@ again:
 			swapcount = swap_count(map[offset + i]);
 			_total_swapcount += swapcount;
 		}
-		map_swapcount = max(map_swapcount, mapcount + swapcount);
+		if (thp_idx < 0 || i == thp_idx)
+			map_swapcount = max(map_swapcount,
+					    mapcount + swapcount);
 	}
 	if (PageDoubleMap(page)) {
 		map_swapcount -= 1;
@@ -1690,7 +1696,7 @@ again:
  *
  * The caller has to check PageKsm before calling this.
  */
-bool can_read_pin_swap_page(struct page *page)
+bool can_read_pin_swap_page(struct page *page, int thp_idx)
 {
 	VM_BUG_ON_PAGE(!PageLocked(page), page);
 	VM_BUG_ON_PAGE(PageKsm(page), page);
@@ -1721,7 +1727,7 @@ bool can_read_pin_swap_page(struct page *page)
 	 */
 	if (PageWriteback(page))
 		return false;
-	return page_trans_huge_map_swapcount(page, NULL, NULL) <= 1;
+	return page_trans_huge_map_swapcount(page, thp_idx, NULL, NULL) <= 1;
 }
 
 /*
@@ -1733,15 +1739,19 @@ bool can_read_pin_swap_page(struct page *page)
  * NOTE: total_map_swapcount should not be relied upon by the caller if
  * reuse_swap_page() returns false, but it may be always overwritten
  * (see the other implementation for CONFIG_SWAP=n).
+ *
+ * If thp_idx is negative check if the whole THP can be reused,
+ * otherwise check only the provided subpage index.
  */
-bool reuse_swap_page(struct page *page, int *total_map_swapcount)
+bool reuse_swap_page(struct page *page, int thp_idx,
+		     int *total_map_swapcount)
 {
 	int count, total_mapcount, total_swapcount;
 
 	VM_BUG_ON_PAGE(!PageLocked(page), page);
 	if (unlikely(PageKsm(page)))
 		return false;
-	count = page_trans_huge_map_swapcount(page, &total_mapcount,
+	count = page_trans_huge_map_swapcount(page, thp_idx, &total_mapcount,
 					      &total_swapcount);
 	if (total_map_swapcount)
 		*total_map_swapcount = total_mapcount + total_swapcount;